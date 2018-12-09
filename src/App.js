880ebd6335128f2ee7a7a4b7bc5defc5f6159539
Vishwajeet Srivastava <vishwajeets912@gmail.com>
2018-12-09 21:03:11 +0100

    mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from './logo.svg';
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    fromCurrency: "EUR",
+    toCurrency: "USD",
+    amount: 1,
+    result: null
+  };
+  handleCurrency = event => {
+    if (event.target.name === "fromcurrency") {
+      this.setState({ fromCurrency: event.target.value });
+    }
+    if (event.target.name === "tocurrency") {
+      this.setState({ toCurrency: event.target.value });
+    }
+    // console.log(event.target.value);
+    // this.setState({ total: event.target.value });
+  };
+
+  handleInput = event => {
+    this.setState({ amount: event.target.value });
+  };
+  convertHandler = () => {
+    if (this.state.fromCurrency !== this.state.toCurrency) {
+      axios
+        .get(
+          `https://api.exchangeratesapi.io/latest?base=${
+            this.state.fromCurrency
+          }&symbols=${this.state.toCurrency}`
+        )
+        .then(response => {
+          const result =
+            this.state.amount * response.data.rates[this.state.toCurrency];
+          this.setState({ result: result.toFixed(5) });
+        })
+        .catch(err => {
+          console.log("Opps", err.message);
+        });
+    } else {
+      this.setState({ result: "You cant convert the same currency!" });
+    }
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <div className="enter_amount">
+          <input
+            type="text"
+            name="amount"
+            value={this.state.amount}
+            onChange={this.handleInput}
+          />
+          <select
+            name="fromcurrency"
+            value={this.state.fromCurrency}
+            onChange={this.handleCurrency}
           >
-            Learn React
-          </a>
-        </header>
+            <option value="USD">USD</option>
+            <option value="EUR">Euro</option>
+            <option value="JPY">Yen</option>
+          </select>
+          <select
+            name="tocurrency"
+            value={this.state.toCurrency}
+            onChange={this.handleCurrency}
+          >
+            <option value="USD">USD</option>
+            <option value="EUR">Euro</option>
+            <option value="JPY">Yen</option>
+          </select>
+          <button onClick={this.convertHandler}>Convert</button>
+        </div>
+        <h3>{this.state.result}</h3>
       </div>
     );
   }
