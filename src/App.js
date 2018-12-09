571c5fba0328b4210b9883cd4925dc1c4a6056ea
Vishwajeet Srivastava <vishwajeets912@gmail.com>
2018-12-09 21:33:26 +0100

    added inital ui with state and event handlers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    fromCurrency: "EUR",
+    toCurrency: "USD",
+    amount: 1,
+    result: null
+  };
+
   render() {
     return (
       <div className="App">
-        
+        <div className="App">
+          <div className="enter_amount">
+            <input
+              type="text"
+              name="amount"
+              value={this.state.amount}
+              onChange={this.handleInput}
+            />
+            <select
+              name="fromcurrency"
+              value={this.state.fromCurrency}
+              onChange={this.handleCurrency}
+            >
+              <option value="USD">US Dollar</option>
+              <option value="EUR">Euro</option>
+              <option value="JPY">Yen</option>
+            </select>
+            <select
+              name="tocurrency"
+              value={this.state.toCurrency}
+              onChange={this.handleCurrency}
+            >
+              <option value="USD">US Dollar</option>
+              <option value="EUR">Euro</option>
+              <option value="JPY">Yen</option>
+            </select>
+            <button onClick={this.convertHandler}>Convert</button>
+          </div>
+          <h3>{this.state.result}</h3>
+        </div>
       </div>
     );
   }
